a5da38336945497e65080ee1816e761609440aa6
Somya2425 <somya36056@gmail.com>
2026-03-18 17:01:32 +0530

    Account Entity Added

--- src/main/java/com/example/Capgi_BankManagementSystem/entity/Account.java
+++ src/main/java/com/example/Capgi_BankManagementSystem/entity/Account.java
@@ -1,8 +1,24 @@
 
 package com.example.Capgi_BankManagementSystem.entity;
 
+import com.example.Capgi_BankManagementSystem.enums.AccountType;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
 
 public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+    private AccountType accountType;
+    private double balance;
 
-
+    @ManyToOne
+    private Customer customer;
+    @OneToMany(mappedBy = "account", cascade = CascadeType.ALL)
+    private List<Loan> loans;
+    @OneToMany(mappedBy = "account", cascade = CascadeType.ALL)
+    private List<Transaction> transactions;
 }
